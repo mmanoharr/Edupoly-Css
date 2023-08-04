701a0d1ff8253ef018af6d42e24f9aa3b6cab057
manohar <mmanoharr@gmail.com>
2023-08-04 15:54:39 +0530

    #js basics

---
+++ script.js
@@ -0,0 +1,49 @@
+//Write a program to print the following output for the given values a=10,b=20 Output expected is: 10+20=30 10-20=-10 10*20=200 10/20=0 10%20=10
+// let a = 10;
+// let b= 20;
+// let c = a / b;
+// console.log(a+"/"+b+"="+parseInt(c));
+// document.write(c);
+//Write a program to print the following for a given value a=3; Output expected: 3*1=3 3*2=6 3*3=9 3*4=12 3*5=15 3*6=18
+//Given a variable var x = 23. Increment its value by 1
+// let x = 23;
+// x += 1;
+// console.log(x);
+//Print the sum of 12 and 23 using the addition operator.
+// let a=12;
+// let b=24;
+// let c = a+b;
+// console.log(c);
+//Print the difference of 52 and 36 using the subtraction operator.
+// let a=52;
+// let b=36;
+// let c = a-b;
+// console.log(c);
+//Print the difference of 52 and 36 using the subtraction operator.
+//Print the product of 23 and 56 using the multiplication operator.
+// let a=23;
+// let b=56;
+// let c = a * b;
+// console.log(c);
+//Divide 36 by 5 and print the output by using the division operator.
+// let a=36;
+// let b=5;
+// let c = a/b;
+// console.log(c);
+//Divide 67 by 4 and print the quotient.
+// let a = 67;
+// let b = 4;
+// let c = a/b;
+// console.log(parseInt(c));
+//Divide 48 by 7 and print the remainder.
+
+// let a = 67;
+// let b = 4;
+// let c = a%b;
+// console.log(c);
+//Print the value of 2 raised to the power of 6.
+// let a = 2;
+// let b = 6;
+// let c = a**b;
+// console.log(c);
+//
